45008585da698e8e1ae41f00f586839f0ff63532
fahad <ranafahad10031@gmail.com>
2021-05-05 18:28:15 +0500

    Exercise:2 Defined a Course model:title,description

--- models/exampleModel.py
+++ models/exampleModel.py
@@ -4,3 +4,8 @@ class minimalModel(models.Model):
     _name='first_model'
     _description = "this is my first model"
     name=fields.Char(requirments=True)
+
+class Course(models.Model):
+    _name = "openacademy.course"
+    name=fields.Char(String='title',required=True)
+    description = fields.Text()
