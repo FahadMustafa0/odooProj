1233b222b24dbefa89f57bd129f7a3c7966c5f2f
fahad <ranafahad10031@gmail.com>
2021-05-06 06:25:59 +0500

    established One2many relation between course and sesson course_id field

--- models/exampleModel.py
+++ models/exampleModel.py
@@ -13,8 +13,9 @@ class Course(models.Model):
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
 class Session(models.Model):
-    _name = 'openacademy.session'
 
+    _name = 'openacademy.session'
+    
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
